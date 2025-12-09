83bb81ce973eed45dc3734f21cf6332a5a479793
EleazarDev <eleazarjarquincv@gmail.com>
2025-12-09 12:57:35 -0600

    start: initial config

---
+++ dba-eureka/src/main/java/com/unsis/dba_eureka/DbaEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.unsis.dba_eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbaEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbaEurekaApplication.class, args);
+	}
+
+}
---
+++ dba-gateway/src/main/java/com/unsis/dba_gateway/DbaGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.unsis.dba_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbaGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbaGatewayApplication.class, args);
+	}
+
+}
